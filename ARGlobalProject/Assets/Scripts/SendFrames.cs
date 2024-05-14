de43f6bb3d7e941e0c865139eef67fc398dbb9ad
Kefabd <abdoukefsi@gmail.com>
2024-05-15 00:21:41 +0100

    first commit

---
+++ ARGlobalProject/Assets/Scripts/SendFrames.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SendFrames : MonoBehaviour
+{
+    // URL of the Django server endpoint to receive the image data
+    private string serverURL = "http://127.0.0.1:8000/app/upload-image/";
+
+    private void Start()
+    {
+        
+    }
+
+    public void SendImage(byte[] imageData)
+    {
+        StartCoroutine(SendImageToServer(imageData));
+    }
+
+    IEnumerator SendImageToServer(byte[] imageData)
+    {
+        // Create a new form to send the image data
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("image", imageData, "captured_image.png", "image/png");
+
+        // Send the form to the server
+        UnityWebRequest request = UnityWebRequest.Post(serverURL, form);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Failed to send image to server: " + request.error);
+        }
+        else
+        {
+            Debug.Log("Image sent successfully!");
+            // Handle the response from the server
+            string responseText = request.downloadHandler.text;
+            if (!string.IsNullOrEmpty(responseText))
+            {
+                // Parse the JSON response
+                Response jsonResponse = JsonUtility.FromJson<Response>(responseText);
+
+                Debug.Log(jsonResponse.message);
+            }
+            else
+            {
+                Debug.LogError("Empty response from the server.");
+            }
+
+        }
+    }
+}
+
+public class Response
+{
+    public string message;
+}
---
+++ Web_Service/backEnd/app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
+import uuid
+
+def index(request):
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+# Create your views here.
+
+@csrf_exempt
+def handle_image_upload(request):
+    
+    if request.method == 'POST' and request.FILES.get('image'):
+         # Process the uploaded image
+        image_file = request.FILES['image']
+    
+        """treatement for the image to be done here
+        # Directory to save the uploaded images
+        save_directory = 'frames/'
+
+        # Ensure the directory exists, create it if not
+        os.makedirs(save_directory, exist_ok=True)
+
+        # Generate a unique filename for the image
+        filename = 'frame_' + str(uuid.uuid4()) + '.png'
+        save_path = os.path.join(save_directory, filename)
+        
+        # Save the image file
+        with open(save_path, 'wb') as f:
+            for chunk in image_file.chunks():
+                f.write(chunk)"""
+        return JsonResponse({'message': 'Image uploaded successfully'})
+    else:
+        return JsonResponse({'message': 'Image upload failed'}, status=400)
+
